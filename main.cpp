de8347cdc073f41703fb4354f66bd193b18511c5
JMH2323 <128647462+JMH2323@users.noreply.github.com>
2024-04-02 10:29:22 -0400

    Adding Camera, Cleaning Main

--- main.cpp
+++ main.cpp
@@ -1,45 +1,14 @@
 
 
 #include "WeakRaytrace.h"
-#include "color.h"
-#include "hittable.h"
+
+#include "camera.h"
 #include "hittable_list.h"
 #include "sphere.h"
 
 #include <iostream>
 
-////// Environment (colors, shapes) //////
-
-
-
-
-
-color ray_color(const ray& r, const hittable& world) {
-
-    // If ray hits an object, return its color
-    hit_record rec;
-    if (world.hit(r, interval(0,infinity), rec)) {
-        return 0.5 * (rec.normal + color(1,1,1));
-    }
-
-    // Setting the back color which the ray will return using lerp (linear interpolation)
-    // The lerp function: blendedValue = (1 - a) * startValue + a * endValue
-    vec3 unit_direction = unit_vector(r.direction());
-    auto a = 0.5*(unit_direction.y() + 1.0);
-    return (1.0-a)*color(1.0, 1.0, 1.0) + a*color(0.5,0.7,1.0);
-
-}
-
 int main() {
-
-    ////// Image //////
-
-    auto aspect_ratio = 16.0 / 9.0;
-    int image_width = 800;
-
-    // Calculate the image height, and ensure that it's at least 1.
-    int image_height = static_cast<int>(image_width / aspect_ratio);
-    image_height = (image_height < 1) ? 1 : image_height;
 
 
     ////// World //////
@@ -51,46 +20,15 @@ int main() {
 
     ////// Camera //////
 
-    auto focal_length = 1.0;
+    camera cam;
 
-    // Viewport widths less than one are ok since they are 'real' valued.
-    auto viewport_height = 2.0;
-    auto viewport_width = viewport_height * (static_cast<double>(image_width)/image_height);
+    ////// Image //////
 
-    auto camera_center = point3(0,0,0);
-
-    // Calculate vectors across horizontal and down vertical viewpoint edges
-    auto viewport_u = vec3(viewport_width, 0, 0);
-    auto viewport_v = vec3(0, -viewport_height, 0);
-
-    // Calculate the horizontal and vertical delta vectors (done on the pixel-level)
-    auto pixel_delta_u = viewport_u / image_width;
-    auto pixel_delta_v = viewport_v / image_height;
-
-    // Calculate the location of the upper left pixel
-    auto viewport_upper_left = camera_center - vec3(0, 0, focal_length) - viewport_u/2 - viewport_v/2;
-    auto pixel100_loc = viewport_upper_left + 0.5 * (pixel_delta_u + pixel_delta_v);
-
-
+    cam.aspect_ratio = 16.0 /9.0;
+    cam.image_width = 400;
 
     ////// Render //////
 
-    std::cout << "P3\n" << image_width << ' ' << image_height << "\n255\n";
+   cam.render(world);
 
-    for (int j = 0; j < image_height; ++j) {
-        std::clog << "\rScanlines remaining: " << (image_height - j) << ' ' << std::flush;
-        for (int i = 0; i < image_width; ++i) {
-            auto pixel_center = pixel100_loc + (i * pixel_delta_u) + (j * pixel_delta_v);
-
-            // Set ray direction and cast the ray
-            // Ray direction is not a unit vector for simplicity and speed.
-            auto ray_direction = pixel_center - camera_center;
-            ray r(camera_center, ray_direction);
-
-            color pixel_color = ray_color(r, world);
-            write_color(std::cout, pixel_color);
-        }
-    }
-
-    std::clog << "\r Done                  \n";
-}
+   }
